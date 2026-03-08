4880830f7771567b63462c6ac166d5aa918fd03b
Kennedy <kennedyjs1624@gmail.com>
2026-03-08 14:55:09 -0300

    Primeiro upload de arquivo
    One files upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'kennedy-v6';
+const ASSETS = ['./index.html','./manifest.json','./icon.png','./icon-192.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  // Força ativação imediata sem esperar fechar abas antigas
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k))) // Apaga TODO o cache antigo
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+        return res
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
