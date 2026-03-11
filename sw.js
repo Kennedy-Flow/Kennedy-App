b1a3b3c2516cd029931ce35b73185a3bb23bd4e6
Kennedy <kennedyjs1624@gmail.com>
2026-03-10 22:21:19 -0300

    Update cache version from v12 to v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kennedy-v12';
+const CACHE = 'kennedy-v13';
 const ASSETS = ['./index.html','./manifest.json','./icon.png','./icon-192.png'];
 
 self.addEventListener('install', e => {
