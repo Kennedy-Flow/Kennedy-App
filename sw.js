8d6026bff63555b2a6b268fd14e1ef21b471b759
Kennedy <kennedyjs1624@gmail.com>
2026-03-11 20:01:19 -0300

    Update cache version from v19.2 to v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kennedy-v19.2';
+const CACHE = 'kennedy-v20';
 const ASSETS = ['./index.html','./manifest.json','./icon.png','./icon-192.png'];
 
 self.addEventListener('install', e => {
