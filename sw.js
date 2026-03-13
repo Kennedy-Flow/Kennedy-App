8017eb93fa8a036f5ce0a93a3c8abf439150898f
Kennedy <kennedyjs1624@gmail.com>
2026-03-13 08:00:34 -0300

    Update cache version to kennedy-v28.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kennedy-v28.1';
+const CACHE = 'kennedy-v28.2';
 // index.html NUNCA entra no cache — sempre busca da rede
 const STATIC_ASSETS = ['./manifest.json','./icon.png','./icon-192.png'];
 
